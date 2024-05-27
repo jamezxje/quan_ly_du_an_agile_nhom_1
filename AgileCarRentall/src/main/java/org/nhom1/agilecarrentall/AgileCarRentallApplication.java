18f42e9e4071a1c3a105d2f5547fccaaafb225bf
Tran Xuan Duy <118041118+devxzje@users.noreply.github.com>
2024-05-27 23:09:28 +0700

    project initialization

---
+++ AgileCarRentall/src/main/java/org/nhom1/agilecarrentall/AgileCarRentallApplication.java
@@ -0,0 +1,13 @@
+package org.nhom1.agilecarrentall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileCarRentallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileCarRentallApplication.class, args);
+	}
+
+}
---
+++ AgileCarRentall/src/test/java/org/nhom1/agilecarrentall/AgileCarRentallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nhom1.agilecarrentall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileCarRentallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
